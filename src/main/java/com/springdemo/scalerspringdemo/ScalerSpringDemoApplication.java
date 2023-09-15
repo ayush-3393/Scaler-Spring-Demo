631d758fe1012c477789e774d587007f0e6894c8
ayush-3393 <ayush.sinha3393@gmail.com>
2023-09-15 13:02:32 +0530

    Initialising the project

---
+++ src/main/java/com/springdemo/scalerspringdemo/ScalerSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo.scalerspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerSpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalerSpringDemoApplication.class, args);
+    }
+
+}
